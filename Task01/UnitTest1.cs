affb139c9338add1c6fe931c7b4ae10162092dcc
IlyaDesyatkov <ilyades95@gmail.com>
2016-02-13 02:38:33 +0400

    Addd

---
+++ Task01/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task01
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] listpeople = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Queue<int> list = new Queue<int>(listpeople);
+
+            while (list.Count != 1)
+            {
+                list.Enqueue(list.Dequeue());
+                list.Dequeue();
+            }
+    }
+}
